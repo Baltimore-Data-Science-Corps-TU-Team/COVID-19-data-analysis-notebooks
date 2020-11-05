eccfc2d1ee09d8aa2c507ff96d8ffd9fa2aaabed
arunkulkarni95 <arunkulkarni95@hotmail.com>
2020-11-05 12:16:34 -0500

    fixed zip column not added properly

--- BmoreDataMerger.py
+++ BmoreDataMerger.py
@@ -51,7 +51,7 @@ df['Zip'] = df['Zip'].astype(int)
 
 #merge with latest covid cases
 df = pd.merge_ordered(zipsLatest,df,fill_method='ffill')
-df = df[['Neigh','CSA','MHINC','TotalCOVIDCases']]
+df = df[['Zip','Neigh','CSA','MHINC','TotalCOVIDCases']]
 
 #export as CSV
 df.to_csv('MASTER_MERGED.csv',index=False)
